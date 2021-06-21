c1d97404aea84a3f9dc4fec1fc0f9c665618c6ae
=KayseMca <=madadmca04@gmail.com>
2021-06-20 21:03:44 -0700

    :bulb: added Institutions model

---
+++ hilib/Institutions/model.py
@@ -0,0 +1,63 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel
+from sqlalchemy import Column ,String, Boolean, Integer, DateTime, Text
+from database import Base, engine
+from uuid import uuid4, UUID
+
+# creating Institution class
+
+class Institution(Base):
+    __tablename__ = 'institutions'
+    id = Column(Integer, primary_key=True,  index=True)
+    business_id = Column(str(uuid4()), index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    description = Column(Text)
+    logo = Column(String, default='business_logo.png')
+    phone = Column(Integer, index=True)
+    email = Column(String, index=True)
+    country = Column(String, index=True,nullable=False)
+    city = Column(String, index = True,nullable=False)
+    district = Column(String, index = True,nullable=False)
+    address = Column(String, index=True)
+    category = Column(String, index=True)
+    website_url = Column(String, index=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now)
+    # other attributes
+    # owner,reviews, **rating
+    
+
+# Create Users Table
+Base.metadata.create_all(bind=engine)
+
+# ** Pydantic Models
+
+class InstitutionsBase(BaseModel):
+    class Config:
+        orm_mode = True 
+        
+    name:str
+    description:str
+    phone:int
+    logo:str
+    email:str
+    country:str
+    city:str
+    district:str
+    address:str
+    category:str
+    website_url:Optional[str] = None
+    
+
+class InstituitonRegister(InstitutionsBase):
+    pass
+
+class InstituitonUpdate(InstitutionsBase):
+    created_at:datetime
+
+class InstituitonRead(InstitutionsBase):
+    id : int
+    business_id : str
+    created_at : datetime
+    updated_at : datetime
